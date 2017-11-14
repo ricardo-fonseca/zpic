3f4005de41295c40423b3818a6988cfc7466db27
Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>
2017-11-14 16:45:11 +0000

    Updates the interface of sim_report on EM2D
    * All versions of the code now use the interface `void sim_report( t_simulation* sim )`
    
    Signed-off-by: Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>

--- em2d/main.c
+++ em2d/main.c
@@ -34,36 +34,36 @@ along with the ZPIC Educational code suite. If not, see <http://www.gnu.org/lice
 //#include "input/lwfa.c"
 
 int main (int argc, const char * argv[]) {
-    
+
 	// Initialize simulation
 	t_simulation sim;
 	sim_init( &sim );
-	
+
     // Run simulation
 	int n, i;
 	float t;
-	
+
 	printf("Starting simulation ...\n\n");
-	
+
 	uint64_t t0,t1;
 	t0 = timer_ticks();
-	
+
 	for (n=0,t=0.0; t<=sim.tmax; n++, t=n*sim.dt) {
 		printf("n = %i, t = %f\n",n,t);
 
-		if ( report ( n , sim.ndump ) ) 
-			sim_report( &sim.emf, &sim.current, sim.species );
-		
+		if ( report ( n , sim.ndump ) )
+      sim_report( &sim );
+
 		// Advance particles and deposit current
 		current_zero( &sim.current );
-		for (i = 0; i<sim.n_species; i++) 
+		for (i = 0; i<sim.n_species; i++)
 			spec_advance(&sim.species[i], &sim.emf, &sim.current );
-		
+
 		// Update current boundary conditions and advance iteration
 		current_update( &sim.current );
-		
+
 		// Advance EM fields
-		emf_advance( &sim.emf, &sim.current );		
+		emf_advance( &sim.emf, &sim.current );
 	}
 
 	t1 = timer_ticks();
@@ -71,9 +71,9 @@ int main (int argc, const char * argv[]) {
 
 	// Simulation times
     sim_timings( &sim, t0, t1 );
-	
+
     // Cleanup data
     sim_delete( &sim );
-    
+
 	return 0;
 }
