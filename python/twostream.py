fc5fbea560814236356155a09ed5e59589fcf6ed
Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>
2018-01-04 17:28:06 +0000

    Adds LWFA test script
    * Also updates two stream examples to match the new syntax
    
    Signed-off-by: Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>

--- python/twostream.py
+++ python/twostream.py
@@ -8,22 +8,19 @@ tmax = 50.0
 
 ndump = 10
 
-d = em1d.Density()
-d.n = 1.0
-
 ppc = 500
-ufl = np.array([0.2,    0.0,  0.0], dtype=np.float32)
-uth = np.array([0.001,0.001,0.001], dtype=np.float32)
+ufl = [0.2,    0.0,  0.0]
+uth = [0.001,0.001,0.001]
 
-right = em1d.Species( "right", -1.0, ppc, ufl, uth, d )
+right = em1d.Species( "right", -1.0, ppc, ufl = ufl, uth = uth )
 
 ufl[0] = -ufl[0]
-left  = em1d.Species( "left", -1.0, ppc, ufl, uth, d )
+left  = em1d.Species( "left", -1.0, ppc, ufl = ufl, uth = uth )
 
 sim = em1d.Simulation( nx, box, dt, tmax, [right,left])
 
-def report( sim, n ):
-    if ( n % ndump == 0 ):
+def report( sim ):
+    if ( sim.n % ndump == 0 ):
         right.report(em1d.Species.particles)
         left.report(em1d.Species.particles)
         sim.emf.report(em1d.EMF.efld,0)
