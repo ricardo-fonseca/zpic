9ead3cbe6b765c12f863cd91b832fdeb1baa0d95
Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>
2018-01-04 17:16:46 +0000

    Improves compile options for python wrapper
    * Disables debug symbols and increases optimization level
    
    Signed-off-by: Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,15 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
+from distutils import sysconfig
+
+# Removes the debug options and replaces the optimization flag
+cflags = sysconfig.get_config_var('CFLAGS')
+cflags = cflags.replace(' -g', '')
+cflags = cflags.replace(' -DNDEBUG', '')
+cflags = cflags.replace('-O3', '-Ofast')
+sysconfig._config_vars['CFLAGS'] = cflags
+
 ext = Extension("em1d",
                 sources=["em1d.pyx",
                 "em1d/current.c",
@@ -9,8 +18,8 @@ ext = Extension("em1d",
 				"em1d/random.c",
 				"em1d/simulation.c",
 				"em1d/timer.c",
-				"em1d/zdf.c"
-])
+				"em1d/zdf.c"]
+)
 
 setup(name="zpic_em1d",
       ext_modules = cythonize([ext]))
