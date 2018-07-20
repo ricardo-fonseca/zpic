5ed5ba2f2396940905668e245f8ed9ac6355685c
Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>
2018-07-20 16:21:37 -0700

    Adds definition of M_PI* constants
    * ANSI C does not define these constants so compilation was failing on some systems
    
    Signed-off-by: Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>

--- em2d/zpic.h
+++ em2d/zpic.h
@@ -19,5 +19,18 @@ typedef struct {
 	t_fld x, y, z;
 } t_vfld;
 
+/* ANSI C does not define math constants */
+
+#ifndef M_PI
+#define M_PI        3.14159265358979323846264338327950288   /* pi             */
+#endif
+
+#ifndef M_PI_2
+#define M_PI_2      1.57079632679489661923132169163975144   /* pi/2           */
+#endif
+
+#ifndef M_PI_4
+#define M_PI_4      0.785398163397448309615660845819875721  /* pi/4           */
+#endif
 
 #endif
