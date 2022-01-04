e7e03296277e7ea087d8076e71931eeb2a477b96
Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>
2022-01-04 17:36:31 +0000

    Fixes compilation of python modules
    Compilation on some systems was broken because a custom compiler was being set (issue #16)

--- python/setup.py
+++ python/setup.py
@@ -14,9 +14,6 @@ cflags += ' -std=c99'
 
 sysconfig._config_vars['CFLAGS'] = cflags
 
-# Set custom compiler
-sysconfig._config_vars['CC'] = 'gcc-8.2.0'
-
 # Finit difference Electromagnetic codes
 em1d = Extension("em1d",
                 sources=["em1d.pyx",
