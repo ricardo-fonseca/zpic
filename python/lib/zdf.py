e9556f84ee9e6e930e111e103e12318c99845a1b
Ricardo Fonseca <ricardo.fonseca@tecnico.ulisboa.pt>
2022-01-25 19:39:46 +0000

    Documents zdf python module

--- python/lib/zdf.py
+++ python/lib/zdf.py
@@ -1,4 +1,6 @@
 """
+Python module for reading ZDF data files
+
 Copyright (C) 2017 Instituto Superior Tecnico
 
 This file is part of the ZPIC Educational code suite
@@ -17,18 +19,107 @@ You should have received a copy of the GNU Affero General Public License
 along with the ZPIC Educational code suite. If not, see <http://www.gnu.org/licenses/>.
 """
 
-#!/usr/bin/python
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
+
+class ZDF_Record:
+    """ZDF_Record()
+
+    ZDF Datafile record information
+
+    Attributes
+    ----------
+    pos : int64
+        Position in ZDF file
+    id : int
+        Type id
+    name : str
+        Record name
+    len : uint64
+        Additional record length in bytes
+    """
+    def __init__( self, pos, id, name, len ):
+        self.pos   = pos
+        self.id    = id
+        self.name  = name
+        self.len   = len
+    
+    def version( self ):
+        """version()
+
+        Gets record version number
+
+        Returns
+        -------
+        version : int
+            Record version number
+        """
+        return self.id & 0x0000FFFF
+
+    def type( self ):
+        """type( typeTag )
+
+        Gets record type from tag
+
+        Returns
+        -------
+        type : {'int', 'double', 'string', 'dataset', 'iteration', 'grid_info', 'part_info','unknown'}
+            Type of record
+        """
+
+        typeID = self.id & 0xFFFF0000
+
+        types = {0x00010000: "int",
+                 0x00020000: "double",
+                 0x00030000: "string",
+                 0x00100000: "dataset",
+                 0x00200000: "iteration",
+                 0x00210000: "grid_info",
+                 0x00220000: "part_info", }
+
+        if (typeID in types):
+            return types[typeID]
+        else:
+            return "unknown"
+
 
 class ZDF_Iteration:
+    """ZDF_Iteration()
+    
+    Class describing iteration information.
+
+    Attributes
+    ----------
+    n : int
+        Iteration value
+    t : float
+        Time value
+    tunits : str
+        Units used for time value
+    """
     def __init__( self ):
         self.n = 0
-        self.t = 0.
+        self.t = 0.0
         self.tunits = ""
 
 class ZDF_Grid_Axis:
+    """ZDF_Grid_Axis()
+
+    Class describing grid axis
+
+    Attributes
+    ----------
+    type : {0,1,2}
+        Axis type, must be one of 0 (linear), 1 (log10) or 2 (log2)
+    min : float
+        Minimum axis value
+    max : float
+        Maximum axis value
+    label : str
+        Axis label
+    units : str
+        Axis values data units
+    """
     def __init__( self ):
         self.type = 0
         self.min = 0.
@@ -37,6 +128,25 @@ class ZDF_Grid_Axis:
         self.units = ''
 
 class ZDF_Grid_Info:
+    """ZDF_Grid_Info()
+
+    Grid dataset information
+
+    Attributes
+    ----------
+    ndims : int
+        Dimensionality of dataset
+    nx : list of int (ndims)
+        Number of grid points in each direction
+    label : str
+        Dataset label
+    units : str
+        Dataset units
+    has_axis : bool
+        True if the dataset includes axis information
+    axis : list of ZDF_Grid_Axis (ndims)
+        Information on each axis, when available
+    """
     def __init__( self ):
         self.ndims = 0
         self.nx = []
@@ -46,6 +156,23 @@ class ZDF_Grid_Info:
         self.axis = []
 
 class ZDF_Part_Info:
+    """ZDF_Part_Info()
+
+    Particle dataset information
+
+    Attributes
+    ----------
+    name : str
+        Particle dataset name
+    nquants : int
+        Number of quantities per particle
+    quants : list of str (nquants)
+        Name of individual quantities
+    units : dictionary
+        Units for each quantity
+    nparts: int
+        Number of particles in dataset
+    """
     def __init__( self ):
         self.name = ''
         self.nquants = 0
@@ -54,6 +181,15 @@ class ZDF_Part_Info:
         self.nparts = 0
 
 class ZDFfile:
+    """ZDFfile( file_name )
+    
+    ZDF data file class
+
+    Parameters
+    ----------
+    file_name : str
+        File name of ZDF data file, should include path
+    """
     def __init__(self, file_name):
         self.__file = open(file_name, "rb")
 
@@ -64,6 +200,10 @@ class ZDFfile:
             self.__file.close
 
     def close(self):
+        """close()
+
+        Closes ZDF file
+        """
         self.__file.close
 
     def __read_uint32(self):
@@ -105,28 +245,21 @@ class ZDFfile:
 
         return fstring
 
-    def record_type(self, typeTag):
-        version = typeTag & 0x0000FFFF
-        typeID = typeTag & 0xFFFF0000
-
-        types = {0x00010000: "int",
-                 0x00020000: "double",
-                 0x00030000: "string",
-                 0x00100000: "dataset",
-                 0x00200000: "iteration",
-                 0x00210000: "grid_info",
-                 0x00220000: "part_info", }
-
-        if (typeID in types):
-            return types[typeID]
-        else:
-            return "unknown"
-
 # -----------------------------------------------------------------------------
 # Low level interfaces
 # -----------------------------------------------------------------------------
 
     def read_record(self, skip=False):
+        """read_record(skip=False)
+
+        Reads current record information from file
+
+        Parameters
+        ----------
+        skip : bool, optional
+            If set to True, skip to next record after reading record
+            header data
+        """
 
         pos = self.__file.tell()
 
@@ -138,23 +271,40 @@ class ZDFfile:
             return False
 
         # Read record ID
-        rec = dict()
-        rec['pos'] = pos
-        rec['id']  = id
-        rec['name'] = self.__read_string()
-        rec['len']  = self.__read_uint64()
+        name = self.__read_string()
+        len = self.__read_uint64()
+        rec = ZDF_Record( pos, id, name, len )
 
+        # If requested, skip over to next record
         if (skip):
-            self.__file.seek(rec['len'], 1)
+            self.__file.seek(rec.len, 1)
 
         return rec
 
     def read_string(self):
+        """read_string()
+
+        Reads string record from data file
+
+        Returns
+        -------
+        string : str
+            String data
+        """
         rec = self.read_record()
         fstring = self.__read_string()
         return fstring
 
     def read_iteration(self):
+        """read_iteration()
+
+        Read iteration record from data file
+
+        Returns
+        -------
+        iteration : ZDF_Iteration()
+            Iteration data
+        """
         rec = self.read_record()
         iteration = ZDF_Iteration()
         iteration.n = self.__read_int32()
@@ -163,6 +313,15 @@ class ZDFfile:
         return iteration
 
     def read_grid_info(self):
+        """read_grid_info()
+
+        Read grid information record from data file
+
+        Returns
+        -------
+        iteration : ZDF_Grid_Info()
+            Grid information data
+        """
         rec = self.read_record()
         info = ZDF_Grid_Info()
         info.ndims = self.__read_uint32()
@@ -187,6 +346,15 @@ class ZDFfile:
         return info
 
     def read_part_info(self):
+        """read_part_info()
+
+        Read particle information record from data file
+
+        Returns
+        -------
+        iteration : ZDF_Part_Info()
+            Iteration data
+        """
         rec = self.read_record()
 
         info = ZDF_Part_Info()
@@ -204,6 +372,15 @@ class ZDFfile:
         return info
 
     def read_dataset(self):
+        """read_dataset()
+
+        Read dataset from data file
+
+        Returns
+        -------
+        data : numpy.ndarray
+            Numpy ndarray with data
+        """
         rec = self.read_record()
 
         data_type = self.__read_int32()
@@ -232,6 +409,16 @@ class ZDFfile:
         return data
 
     def list(self, printRec=False):
+        """list( printRec=False )
+
+        Gets a list of file contents and optionally prints it to screen
+
+        Parameters
+        ----------
+        printRec : bool, optional
+            If set to True will print all records found in the file,
+            defaults to False.
+        """
         # Position file after magic number
         self.__file.seek(4)
 
@@ -248,8 +435,8 @@ class ZDFfile:
             print('-------------------------------------------------')
             for rec in rec_list:
                 print('{:#010x}   {:#010x}   {:10}  {}'.format(
-                    rec['pos'], rec['len'],
-                    self.record_type(rec['id']), rec['name']))
+                    rec.pos, rec.len,
+                    self.record_type(rec.id), rec.name))
 
         return rec_list
 
@@ -258,7 +445,21 @@ class ZDFfile:
 # -----------------------------------------------------------------------------
 
 class ZDF_Info:
-    """ZDF File information"""
+    """ZDF_Info()
+    ZDF File information
+
+    Attributes
+    ----------
+    type : {'grid','particles'}
+        Type of ZDF file
+    grid : ZDF_Grid_Info
+        Grid information for grid files
+    particles : ZDF_Part_Info
+        Particle information for particle files
+    iteration : ZDF_Iteration
+        Iteration information
+
+    """
     def __init__(self):
         self.type = ""
         self.grid = None
@@ -267,6 +468,20 @@ class ZDF_Info:
 
 
 def info( file_name ):
+    """info( file_name )
+
+    Gets metadata for a ZDF file
+
+    Parameters
+    ----------
+    file_name : str
+        File name to use, should include path
+    
+    Returns
+    -------
+    info : ZDF_Info
+        File information. If file is invalid False is returned.
+    """
     # Open file
     zdf = ZDFfile( file_name )
 
@@ -291,6 +506,22 @@ def info( file_name ):
     return info
 
 def read( file_name ):
+    """read( file_name )
+
+    Reads all data in a ZDF file
+
+    Parameters
+    ----------
+    file_name : str
+    
+    Returns
+    -------
+    (data, info) : ( numpy.ndarray | dictionary, ZDF_Info )
+        Tuple containing file data and metadata. Data will be a
+        numpy.ndarray for grid data, and a dictionary of numpy.array for
+        particle data (one entry per quantity). Metadata is returned as a
+        ZDF_Info object. If file is invalid False is returned.
+    """
     # Open file
     zdf = ZDFfile( file_name )
 
